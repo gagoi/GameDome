645c0849e4988ae63cecf021e4dd1f847dc51c7a
apoupene <antoine.poupeney@insa-cvl.fr>
2019-12-24 22:28:09 +0000

    connect client c

--- client/client.py
+++ client/client.py
@@ -4,6 +4,8 @@ import time as t
 MAX = 20
 HOSTd = '2.3.130.203'
 PORTd = 45703
+
+
 # HOSTd = '127.0.0.1'
 # PORTd = 8081
 # HOSTd = '192.168.43.254'
@@ -14,22 +16,23 @@ def main():
     s = connection()
     print("You are now connected, enjoy !\n")
     message = input('Enter your username: ')
-    message = "NC"+message+"\n"
-    n = s.send(bytes(message, 'utf-8'))
-    if (n != len(message )):
-        print('Erreur envoi')
-        return False
+    message = "NC" + message + "\n"
+    envoi(s, message)
 
-    while(1):
-        r = s.recv(MAX)
-        print(r)
-        if r=="S":
-            # LUNCH GAME
-            break
+    envoi(s, "M\n  ")
+    wait(s, 'S')
+    morpion(s)
 
     # t.sleep(3000000)
     print('Deconnexion.')
     s.close()
+
+
+def envoi(s, message):
+    n = s.send(bytes(message, 'utf-8'))
+    if (n != len(message)):
+        print('Erreur envoi')
+        return False
 
 
 def initConnect():
@@ -44,7 +47,8 @@ def initConnect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # s.connect(HOST, PORT)
-    return s,HOST,PORT
+    return s, HOST, PORT
+
 
 def connection():
     s, HOST, PORT = initConnect()
@@ -60,5 +64,53 @@ def connection():
     return s
 
 
+def wait(s, c):
+    while (1):
+        r = s.recv(MAX).decode("utf-8")
+        print(r)
+        if c in r:
+            return r
+
+
+def morpion(s):
+    while (1):
+        r = s.recv(MAX).decode("utf-8")
+        print("recu: ",r)
+        if "GG" in r:
+            print("Well done!")
+            return 0;
+        if "L" in r:
+            print("You loooose...")
+            return 0;
+        elif "G" in r:
+            if "T" in r:
+                grilleMor(r[2:] + "\n")
+                n = input("Your turn, where to play: ")
+                n = "P" + str(n) + "\n"
+                envoi(s, n)
+            else:
+                grilleMor(r[2:] + "\n")
+
+
+        elif "T" in r:
+            r2 = s.recv(MAX).decode("utf-8")
+            print("recu: ", r2)
+            grilleMor(r2[1:])
+            n=""
+            n = input("Your turn, where to play: ")
+            n = "P" + str(n) + "\n"
+            print(n)
+            envoi(s, n)
+
+
+
+
+
+def grilleMor(g):
+    print(g[:3])
+    print("-----")
+    print(g[4:6])
+    print("-----")
+    print(g[7:9])
 
 main()
