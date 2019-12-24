1bddc4818ce9a345fb70c719e11a5525ef88ceef
apoupene <antoine.poupeney@insa-cvl.fr>
2019-12-24 19:01:42 +0000

    connect client c

--- client/client.py
+++ client/client.py
@@ -1,24 +1,64 @@
 import socket
 import time as t
 
-# HOST = '127.0.0.1'
-# PORT = 8081
-HOST = '192.168.43.254'
-PORT = 45703
+MAX = 20
+HOSTd = '2.3.130.203'
+PORTd = 45703
+# HOSTd = '127.0.0.1'
+# PORTd = 8081
+# HOSTd = '192.168.43.254'
+# PORTd = 45703
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
-print('Connexion vers ' + HOST + ':' + str(PORT) + ' reussie.')
+def main():
+    print("[--  Hello  --]")
+    s = connection()
+    print("You are now connected, enjoy !\n")
+    message = input('Enter your username: ')
+    message = "NC"+message+"\n"
+    n = s.send(bytes(message, 'utf-8'))
+    if (n != len(message )):
+        print('Erreur envoi')
+        return False
 
-message = 'NCnyrvana\n'
-print('Envoi de :' + message)
-n = client.send(bytes(message, 'utf-8'))
-if (n != len(message)):
-    print( 'Erreur envoi.')
-else:
-    print( 'Envoi ok.')
+    while(1):
+        r = s.recv(MAX)
+        print(r)
+        if r=="S":
+            # LUNCH GAME
+            break
 
-client.send(bytes("message\n", 'utf-8'))
-t.sleep(3000000)
-print('Deconnexion.')
-client.close()
+    # t.sleep(3000000)
+    print('Deconnexion.')
+    s.close()
+
+
+def initConnect():
+    HOST = input("Enter server host (default" + HOSTd + "): ")
+    if HOST == "":
+        HOST = HOSTd
+    PORT = input("Enter server port (default" + str(PORTd) + "): ")
+    if PORT == "":
+        PORT = PORTd
+    else:
+        PORT = int(PORT)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # s.connect(HOST, PORT)
+    return s,HOST,PORT
+
+def connection():
+    s, HOST, PORT = initConnect()
+    while (True):
+
+        print(HOST, PORT)
+        try:
+            s.connect((HOST, PORT))
+            break
+        except:
+            print("Unable to connect, try again")
+            s = initConnect()
+    return s
+
+
+
+main()
